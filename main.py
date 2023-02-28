7f4394a59bcf8cd05115ba80226053f8a1b1f64d
The Dogebird <108379190+TheDogeBird@users.noreply.github.com>
2023-02-28 00:24:30 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 import os
+import json
 import random
+import sqlite3
+from datetime import datetime
 import torch
 import transformers
-import json
-import sqlite3
-import datetime
-import threading
-from concurrent.futures import ThreadPoolExecutor
 
-MODELS_DIR = "models"
-DLS_DIR = os.path.join(MODELS_DIR, "dls")
+
+# Define constants
+DLS_DIR = "models/dls"
+BERT_DIR = "models/bert"
 
 
 class ChatGPT:
-    def __init__(self, config_path="config.json", model=None, tokenizer=None):
+    def __init__(self, config_path="config.json", model=None, tokenizer=None, db_path="chathis.db"):
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.config_path = config_path
         # Load config file
         with open(config_path) as f:
             config = json.load(f)
@@ -26,12 +28,13 @@ class ChatGPT:
             self.tokenizer = tokenizer
             self.model = model
         else:
-            self.model_engine = config.get("model_engine", "text-davinci-002")
+            self.model_engine = config.get("model_engine", "gpt2-medium")
             tokenizer_path = os.path.join(DLS_DIR, self.model_engine)
             model_path = os.path.join(DLS_DIR, self.model_engine)
             if not os.path.exists(tokenizer_path) or not os.path.exists(model_path):
                 print("Downloading model...")
-                tokenizer, model = self.download_model(self.model_engine)
+                tokenizer, model = transformers.GPT2Tokenizer.from_pretrained(self.model_engine), \
+                                   transformers.GPT2LMHeadModel.from_pretrained(self.model_engine)
                 tokenizer.save_pretrained(tokenizer_path)
                 model.save_pretrained(model_path)
             else:
@@ -47,89 +50,134 @@ class ChatGPT:
         random.seed(self.seed)
         torch.manual_seed(self.seed)
 
-        # Set up database connection
-        self.db_path = config.get("db_path", "chat_history.db")
-        self.conn = sqlite3.connect(self.db_path)
-        self.cursor = self.conn.cursor()
+        # Set up database
+        self.db_path = db_path
+        self.create_tables()
 
-        # Create table for chat history if it doesn't exist
-        self.cursor.execute("CREATE TABLE IF NOT EXISTS chat_history (id INTEGER PRIMARY KEY AUTOINCREMENT, "
-                            "user_message TEXT, bot_message TEXT, timestamp TEXT)")
+        # Set up BERT for sentiment analysis
+        self.bert_tokenizer, self.bert_model = self.download_bert()
 
-        # Create thread pool executor
-        self.executor = ThreadPoolExecutor(max_workers=2)
+        # Set up device
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    def generate_response(self, user_message):
-        input_ids = self.tokenizer.encode(user_message, return_tensors="pt")
-        response = self.model.generate(input_ids, max_length=50, num_beams=5, no_repeat_ngram_size=2,
-                                       early_stopping=True)
-        bot_message = self.tokenizer.decode(response[0], skip_special_tokens=True)
+    def get_last_user_message(self):
+        conn = sqlite3.connect(self.db_path)
+        c = conn.cursor()
+        c.execute("SELECT user_input FROM chat_history WHERE bot_response IS NOT NULL ORDER BY timestamp DESC LIMIT 1")
+        last_user_message = c.fetchone()
+        conn.close()
+        return last_user_message[0] if last_user_message else None
 
-        # Check if there is a previous conversation in the database
-        previous_conversation = self.cursor.execute("SELECT * FROM chat_history WHERE user_message = ? "
-                                                    "ORDER BY timestamp DESC LIMIT 1",
-                                                    (user_message,)).fetchone()
-        if previous_conversation is not None:
-            # If there is a previous conversation, use the context to generate the response
-            context = previous_conversation[2]  # Column index for bot message
-            input_ids = self.tokenizer.encode(context + user_message, return_tensors="pt")
-            response = self.model.generate(input_ids, max_length=50, num_beams=5, no_repeat_ngram_size=2,
-                                           early_stopping=True)
-            bot_message = self.tokenizer.decode(response[0], skip_special_tokens=True)
-
-        # Store the current conversation in the database
-        timestamp = str(datetime.datetime.now())
-        self.cursor.execute("INSERT INTO chat_history (user_message, bot_message, timestamp) VALUES (?, ?, ?)",
-                            (user_message, bot_message, timestamp))
-        self.conn.commit()
-
-        return bot_message.strip()
-
-    def download_model(self, engine):
-        tokenizer = transformers.GPT2Tokenizer.from_pretrained(engine)
-        model = transformers.GPT2LMHeadModel.from_pretrained(engine)
-        return tokenizer, model
-
-    def chat(self):
-        print("Type 'exit' to end the conversation.")
-        username = input("Enter your name: ")
-        print(f"Hello {username}! Let's start the conversation.")
-
-        # Download and load model
-        model_engine = "text-davinci-002"
-        tokenizer_path = os.path.join(DLS_DIR, model_engine)
-        model_path = os.path.join(DLS_DIR, model_engine)
+    def download_bert(self):
+        bert_engine = "bert-base-uncased"
+        tokenizer_path = os.path.join(BERT_DIR, bert_engine)
+        model_path = os.path.join(BERT_DIR, bert_engine)
         if not os.path.exists(tokenizer_path) or not os.path.exists(model_path):
-            print("Downloading model...")
-            tokenizer, model = self.download_model(model_engine)
+            print("Downloading BERT...")
+            tokenizer = transformers.AutoTokenizer.from_pretrained(bert_engine)
+            model = transformers.AutoModelForSequenceClassification.from_pretrained(bert_engine)
             tokenizer.save_pretrained(tokenizer_path)
             model.save_pretrained(model_path)
         else:
-            print("Loading existing model...")
-            tokenizer = transformers.GPT2Tokenizer.from_pretrained(tokenizer_path)
-            model = transformers.GPT2LMHeadModel.from_pretrained(model_path)
+            print("Loading existing BERT...")
+            tokenizer = transformers.AutoTokenizer.from_pretrained(tokenizer_path)
+            model = transformers.AutoModelForSequenceClassification.from_pretrained(model_path)
 
-        # Initialize chatbot
-        chatbot = ChatGPT(model=model, tokenizer=tokenizer)
+        model.to(self.device)
+        return tokenizer, model
 
-        # Define function for chatbot response generation
-        def get_response():
-            user_message = input(f"{username}: ")
-            bot_message = chatbot.generate_response(user_message)
-            print(f"Chatbot: {bot_message}\n")
+    def create_tables(self):
+        conn = sqlite3.connect(self.db_path)
+        c = conn.cursor()
+        c.execute('''CREATE TABLE IF NOT EXISTS chat_history
+                     (user_input TEXT, bot_response TEXT, sentiment TEXT, timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP)''')
 
-        # Start chatting
-        with ThreadPoolExecutor() as executor:
-            while True:
-                user_input = input("Press enter to continue or type 'exit' to end the conversation: ")
-                if user_input.lower() == "exit":
-                    print("Chat ended.")
-                    break
-                executor.submit(get_response)
+        conn.commit()
+        conn.close()
 
-        # Close database connection
-        self.conn.close()
+    def process_user_input(self, user_input):
+        # Clean user input
+        user_input = user_input.strip().lower()
+        # Remove punctuation and special characters
+        user_input = "".join(c for c in user_input if c.isalnum() or c.isspace())
+
+        return user_input
+
+    def classify_sentiment(self, text):
+        inputs = self.bert_tokenizer(text, padding=True, truncation=True, return_tensors="pt")
+        inputs = inputs.to(self.device)
+        outputs = self.bert_model(**inputs)
+        probs = torch.softmax(outputs.logits, dim=1)
+        sentiment = "positive" if probs[0][1] > 0.5 else "negative"
+        return sentiment
+
+    def generate_response(self, user_input):
+        # Clean user input
+        user_input = self.process_user_input(user_input)
+
+        # Classify sentiment of user message
+        last_user_message = self.get_last_user_message()
+        if last_user_message is None:
+            last_user_sentiment = ""
+        else:
+            last_user_sentiment = self.classify_sentiment(last_user_message)
+
+        # Save user message to database
+        conn = sqlite3.connect(self.db_path)
+        c = conn.cursor()
+        timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        c.execute("INSERT INTO chat_history (user_input, sentiment, timestamp) VALUES (?, ?, ?)",
+                  (user_input, last_user_sentiment, timestamp))
+        conn.commit()
+        conn.close()
+
+        # Generate bot response
+        input_ids = self.tokenizer.encode(user_input, return_tensors="pt")
+        input_ids = input_ids.to(self.device)
+        max_length = random.randint(50, 100)
+        sample_outputs = self.model.generate(
+            input_ids,
+            do_sample=True,
+            max_length=max_length,
+            top_k=50,
+            top_p=0.95,
+            num_return_sequences=1
+        )
+
+        bot_response = self.tokenizer.decode(sample_outputs[0], skip_special_tokens=True)
+
+        # Save bot response to database
+        conn = sqlite3.connect(self.db_path)
+        c = conn.cursor()
+        timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        c.execute("INSERT INTO chat_history (bot_response, sentiment, timestamp) VALUES (?, ?, ?)",
+                  (bot_response, last_user_sentiment, timestamp))
+        conn.commit()
+        conn.close()
+
+        return bot_response
+
+    def save_to_database(self, user_message, bot_response, sentiment):
+        conn = sqlite3.connect(self.db_path)
+        c = conn.cursor()
+        timestamp = str(int(datetime.timestamp(datetime.now())))
+        c.execute("INSERT INTO chat_history VALUES (?, ?, ?, ?)",
+                  (user_message, bot_response, sentiment, timestamp))
+        conn.commit()
+        conn.close()
+
+    def run(self):
+        print("Chatbot started, type 'exit' to exit.")
+        while True:
+            user_input = input("You: ")
+            if user_input.strip().lower() == "exit":
+                break
+            response = self.generate_response(user_input)
+            print("Bot:", response)
 
 if __name__ == "__main__":
-    chatbot = ChatGPT()
-    chatbot.chat()
+    chatbot = ChatGPT(config_path="config.json")
+    if chatbot.device.type == "cuda":
+        torch.cuda.empty_cache()
+    chatbot.run()
+
